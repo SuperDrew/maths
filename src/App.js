d7575b5f0d875e6779cb710076150d68827e5217
Vittorio <vittorio.gue@gmail.com>
2020-03-21 19:32:08 +0000

    add pages and some style

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Generatore di frasi del Mantu
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Generatore di frasi del Mantu
+                </p>
+            </header>
+
+            <div>
+                <div><label className='App-label'>Scegli un numero</label></div>
+                <input className='App-input' type='number'/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
