83058a3aef1b46e2975a96f142babba385dfcfd6
Andrew Fraser <a.vector@gmail.com>
2021-03-20 10:46:52 +0000

    add initial structure

--- src/App.js
+++ src/App.js
@@ -12,6 +12,39 @@ function App() {
             </header>
 
             <div className='App-inputContainer'>
+                <div>
+                    <label htmlFor="min">Minimum: </label>
+                    <select name="min" id="min">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label htmlFor="max">Maximum: </label>
+                    <select name="max" id="max">
+                        <option value="10">10</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label htmlFor="addition">Use addition: </label>
+                    <input type="checkbox" id="addition" name="addition" value="addition"/>
+                </div>
+
+                <div>
+                    <button type="button">Generate maths fun!</button>
+                </div>
+
+                <table id="table">
+                    <tr>
+                    </tr>
+                </table>
             </div>
         </div>
     );
