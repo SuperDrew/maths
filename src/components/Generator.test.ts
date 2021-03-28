de1d5f949968d0b61694689e0d15ec7023d0baa0
Andrew Fraser <a.vector@gmail.com>
2021-03-28 14:50:10 +0100

    add fast check for random number generation

--- src/components/Generator.test.ts
+++ src/components/Generator.test.ts
@@ -1,11 +1,15 @@
 import { randBetween } from './Generator';
+import * as fc from 'fast-check';
 
 describe('Generator', () => {
     it('should generate a random number <= min and <= max', () => {
-        const min = 0;
-        const max = 1;
-        const rand = randBetween(min, max);
-        expect(rand).toBeGreaterThanOrEqual(min);
-        expect(rand).toBeLessThanOrEqual(max);
+        fc.assert(
+            fc.property(fc.nat(), fc.nat(), (nat1, nat2) => {
+                const min = nat1;
+                const max = nat1 + nat2;
+                expect(randBetween(min, max)).toBeLessThanOrEqual(max);
+                expect(randBetween(min, max)).toBeGreaterThanOrEqual(min);
+            })
+        );
     });
 });
