4a6b07a05fa1f3a3ebbf8edbb969e86ac906eb7c
Andrew Fraser <a.vector@gmail.com>
2021-03-28 02:01:18 +0100

    ok but use prettier properly

--- src/App.js
+++ src/App.js
@@ -1,124 +1,124 @@
-import React, { useEffect, useState } from "react";
-import "./reset.css";
-import Button from "@material-ui/core/Button";
-import { Container, Grid, Icon, Paper, Typography } from "@material-ui/core";
-import { LabelledSlider } from "./components/LabelledSlider";
-import { LabelledCheckBox } from "./components/LabelledCheckBox";
-import { QuestionTable } from "./components/QuestionTable";
-import { withStyles } from "@material-ui/styles";
+import React, { useEffect, useState } from 'react';
+import './reset.css';
+import Button from '@material-ui/core/Button';
+import { Container, Grid, Icon, Paper, Typography } from '@material-ui/core';
+import { LabelledSlider } from './components/LabelledSlider';
+import { LabelledCheckBox } from './components/LabelledCheckBox';
+import { QuestionTable } from './components/QuestionTable';
+import { withStyles } from '@material-ui/styles';
 
 const GlobalCss = withStyles({
-  "@global": {
-    "html, body": {
-      backgroundColor: "#f5f5f5",
+    '@global': {
+        'html, body': {
+            backgroundColor: '#f5f5f5',
+        },
     },
-  },
 })(() => null);
 
 function App() {
-  const [min, setMin] = useState(0);
-  const [max, setMax] = useState(10);
-  const [rows, setRows] = useState([]);
-  const updateMin = (changedMin) => {
-    setMin(changedMin);
-  };
+    const [min, setMin] = useState(0);
+    const [max, setMax] = useState(10);
+    const [rows, setRows] = useState([]);
+    const updateMin = (changedMin) => {
+        setMin(changedMin);
+    };
 
-  const updateMax = (changedMax) => {
-    setMax(changedMax);
-  };
+    const updateMax = (changedMax) => {
+        setMax(changedMax);
+    };
 
-  const randBetween = (min, max) => {
-    return Math.round(Math.random() * (max - min) + min);
-  };
+    const randBetween = (min, max) => {
+        return Math.round(Math.random() * (max - min) + min);
+    };
 
-  const generateRandomAdditionSum = (min, max) => {
-    return `${randBetween(min, max)} + ${randBetween(min, max)} = ___`;
-  };
+    const generateRandomAdditionSum = (min, max) => {
+        return `${randBetween(min, max)} + ${randBetween(min, max)} = ___`;
+    };
 
-  const createRow = (min, max, rowNumber) => ({
-    key: rowNumber,
-    sum1: generateRandomAdditionSum(min, max),
-    sum2: generateRandomAdditionSum(min, max),
-    sum3: generateRandomAdditionSum(min, max),
-  });
+    const createRow = (min, max, rowNumber) => ({
+        key: rowNumber,
+        sum1: generateRandomAdditionSum(min, max),
+        sum2: generateRandomAdditionSum(min, max),
+        sum3: generateRandomAdditionSum(min, max),
+    });
 
-  const generateRows = (min, max) => {
-    const rows = [];
-    for (let i = 0; i <= 10; i++) {
-      rows.push(createRow(min, max, i));
-    }
-    setRows(rows);
-  };
+    const generateRows = (min, max) => {
+        const rows = [];
+        for (let i = 0; i <= 10; i++) {
+            rows.push(createRow(min, max, i));
+        }
+        setRows(rows);
+    };
 
-  useEffect(() => {
-    generateRows(min, max);
-  }, [min, max]);
+    useEffect(() => {
+        generateRows(min, max);
+    }, [min, max]);
 
-  return (
-    <div className="App">
-      <GlobalCss />
-      <header className="App-header">
-        <Typography variant="h1" align="center">
-          Maths
-        </Typography>
-      </header>
-      <Container maxWidth="sm">
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Paper>
-              <Grid item>
-                <LabelledSlider
-                  label="minimum"
-                  max={10}
-                  defaultValue={min}
-                  step={1}
-                  onChange={updateMin}
-                />
-              </Grid>
-              <Grid item>
-                <LabelledSlider
-                  label="maximum"
-                  max={10}
-                  defaultValue={max}
-                  step={1}
-                  onChange={updateMax}
-                />
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid container direction="row" justify="center" spacing={2}>
-            <Grid item>
-              <Paper>
-                <LabelledCheckBox name="addition" color="primary" />
-              </Paper>
-            </Grid>
-            <Grid item>
-              <Paper>
-                <LabelledCheckBox name="subtraction" color="primary" />
-              </Paper>
-            </Grid>
-          </Grid>
-          <Grid item justify="center" spacing={1}>
-            <Paper>
-              <Button
-                variant="contained"
-                color="primary"
-                endIcon={<Icon>send</Icon>}
-                onClick={() => {
-                  generateRows(min, max);
-                }}
-              >
-                Generate maths fun!
-              </Button>
-            </Paper>
-          </Grid>
-          <Grid>
-            <QuestionTable label="Questions" rows={rows} />
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <GlobalCss />
+            <header className="App-header">
+                <Typography variant="h1" align="center">
+                    Maths
+                </Typography>
+            </header>
+            <Container maxWidth="sm">
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Paper>
+                            <Grid item>
+                                <LabelledSlider
+                                    label="minimum"
+                                    max={10}
+                                    defaultValue={min}
+                                    step={1}
+                                    onChange={updateMin}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <LabelledSlider
+                                    label="maximum"
+                                    max={10}
+                                    defaultValue={max}
+                                    step={1}
+                                    onChange={updateMax}
+                                />
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid container direction="row" justify="center" spacing={2}>
+                        <Grid item>
+                            <Paper>
+                                <LabelledCheckBox name="addition" color="primary" />
+                            </Paper>
+                        </Grid>
+                        <Grid item>
+                            <Paper>
+                                <LabelledCheckBox name="subtraction" color="primary" />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                    <Grid item justify="center" spacing={1}>
+                        <Paper>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                endIcon={<Icon>send</Icon>}
+                                onClick={() => {
+                                    generateRows(min, max);
+                                }}
+                            >
+                                Generate maths fun!
+                            </Button>
+                        </Paper>
+                    </Grid>
+                    <Grid>
+                        <QuestionTable label="Questions" rows={rows} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
