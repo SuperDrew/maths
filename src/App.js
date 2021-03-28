0c09aa9e1800e2a64875309f9bc581c593283c09
Andrew Fraser <a.vector@gmail.com>
2021-03-28 00:56:03 +0000

    working on style and added prettier

--- src/App.js
+++ src/App.js
@@ -1,72 +1,120 @@
-import React, {useEffect, useState} from 'react';
-import './reset.css';
-import Button from '@material-ui/core/Button';
-import {Container, Divider, Grid, Icon, Typography} from "@material-ui/core";
-import {LabelledSlider} from "./components/LabelledSlider";
-import {LabelledCheckBox} from "./components/LabelledCheckBox";
-import {QuestionTable} from "./components/QuestionTable";
-import {useStyles} from "./styles/styles";
+import React, { useEffect, useState } from "react";
+import "./reset.css";
+import Button from "@material-ui/core/Button";
+import { Container, Grid, Icon, Paper, Typography } from "@material-ui/core";
+import { LabelledSlider } from "./components/LabelledSlider";
+import { LabelledCheckBox } from "./components/LabelledCheckBox";
+import { QuestionTable } from "./components/QuestionTable";
+import { withStyles } from "@material-ui/styles";
+
+const GlobalCss = withStyles({
+  "@global": {
+    "html, body": {
+      backgroundColor: "#f5f5f5",
+    },
+  },
+})(() => null);
 
 function App() {
-    const classes = useStyles();
-    const [min, setMin] = useState(0);
-    const [max, setMax] = useState(10);
-    const [rows, setRows] = useState([]);
-    const updateMin = (changedMin) => {setMin(changedMin)};
-    const updateMax = (changedMax) => {setMax(changedMax)};
-    function randBetween(min, max) {
-
-        return Math.round(Math.random()*(max-min)+min);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(10);
+  const [rows, setRows] = useState([]);
+  const updateMin = (changedMin) => {
+    setMin(changedMin);
+  };
+  const updateMax = (changedMax) => {
+    setMax(changedMax);
+  };
+  function randBetween(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+  function generateRandomAdditionSum(min, max) {
+    return `${randBetween(min, max)} + ${randBetween(min, max)} = ___`;
+  }
+  function createRow(min, max, rowNumber) {
+    return {
+      key: rowNumber,
+      sum1: generateRandomAdditionSum(min, max),
+      sum2: generateRandomAdditionSum(min, max),
+      sum3: generateRandomAdditionSum(min, max),
+    };
+  }
+  const generateRows = (min, max) => {
+    const rows = [];
+    for (let i = 0; i <= 10; i++) {
+      rows.push(createRow(min, max, i));
     }
-    function generateRandomAdditionSum(min, max) {
+    setRows(rows);
+  };
+  useEffect(() => {
+    generateRows(min, max);
+  }, [min, max]);
 
-        return `${randBetween(min, max)} + ${randBetween(min, max)} = ___`;
-    }
-    function createRow(min, max, rowNumber) {
-
-        return {
-            key: rowNumber,
-            sum1: generateRandomAdditionSum(min,max),
-            sum2: generateRandomAdditionSum(min,max),
-            sum3: generateRandomAdditionSum(min,max)
-        };
-    }
-    const generateRows = (min, max) => {
-
-        const rows = [];
-        for (let i = 0; i <= 10; i++) {
-            rows.push(createRow(min, max, i))
-        }
-        setRows(rows);
-    }
-    useEffect(() => {generateRows(min, max)}, [min, max]);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <Typography variant="h1" align="center">
-                    Maths
-                </Typography>
-            </header>
-            <Container maxWidth = "sm">
-                <div>
-                    <LabelledSlider label="minimum" max={10} defaultValue={min} step={1} onChange={updateMin}/>
-                    <LabelledSlider label="maximum" max={10} defaultValue={max} step={1} onChange={updateMax}/>
-                </div>
-                <Grid container className={classes.root}>
-                    <LabelledCheckBox name="addition" color="primary"/>
-                    <LabelledCheckBox name="subtraction" color="primary"/>
-                </Grid>
-                <Divider variant="middle"/>
-                <Grid container justify="center">
-                    <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>} onClick={ () => {generateRows(min, max)}}>
-                        Generate maths fun!
-                    </Button>
-                </Grid>
-                <QuestionTable label="Questions" rows={rows}/>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <GlobalCss />
+      <header className="App-header">
+        <Typography variant="h1" align="center">
+          Maths
+        </Typography>
+      </header>
+      <Container maxWidth="sm">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Paper>
+              <Grid item>
+                <LabelledSlider
+                  label="minimum"
+                  max={10}
+                  defaultValue={min}
+                  step={1}
+                  onChange={updateMin}
+                />
+              </Grid>
+              <Grid item>
+                <LabelledSlider
+                  label="maximum"
+                  max={10}
+                  defaultValue={max}
+                  step={1}
+                  onChange={updateMax}
+                />
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid container direction="row" justify="center" spacing={2}>
+            <Grid item>
+              <Paper>
+                <LabelledCheckBox name="addition" color="primary" />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper>
+                <LabelledCheckBox name="subtraction" color="primary" />
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid item justify="center" spacing={1}>
+            <Paper>
+              <Button
+                variant="contained"
+                color="primary"
+                endIcon={<Icon>send</Icon>}
+                onClick={() => {
+                  generateRows(min, max);
+                }}
+              >
+                Generate maths fun!
+              </Button>
+            </Paper>
+          </Grid>
+          <Grid>
+            <QuestionTable label="Questions" rows={rows} />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,16 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
     root: {
-        width: 'fit-content',
         border: `1px solid ${theme.palette.divider}`,
-        borderRadius: theme.shape.borderRadius,
         backgroundColor: theme.palette.background.paper,
         color: theme.palette.text.secondary,
-        '& svg': {
-            margin: theme.spacing(1.5),
-        },
-        '& hr': {
-            margin: theme.spacing(0, 0.5),
-        }
     },
 }));
