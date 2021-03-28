27737bec6f396878e7fe0d4eff9fc044f9f6ea4a
Andrew Fraser <a.vector@gmail.com>
2021-03-28 15:11:26 +0100

    main render test expects nothing to throw

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 import '@testing-library/jest-dom/extend-expect';
 
-test('Renders App with header', async () => {
-    render(<App />);
-    const header = await screen.findByTestId('header');
-    expect(header).toBeInTheDocument();
+describe('render App', () => {
+    it('should not throw any exceptions', () => {
+        expect(render(<App />)).not.toThrow();
+    });
 });
