20b6e4c2987941278da9dc2dfe883a4d0e5f7e46
Andrew Fraser <a.vector@gmail.com>
2021-03-28 15:07:22 +0100

    consistent namign in generator

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -11,21 +11,21 @@ export interface Row {
     sums: string[];
 }
 
-const createSums = (min: number, numberBond: number) => [
+const generateSums = (min: number, numberBond: number) => [
     generateRandomAdditionSum(min, numberBond),
     generateRandomAdditionSum(min, numberBond),
     generateRandomAdditionSum(min, numberBond),
 ];
 
-const createRow = (min: number, numberBond: number, rowNumber: number): Row => ({
+const generateRow = (min: number, numberBond: number, rowNumber: number): Row => ({
     key: rowNumber,
-    sums: createSums(min, numberBond),
+    sums: generateSums(min, numberBond),
 });
 
 export const generateRows = (min: number, numberBond: number, _useAddition: boolean): Row[] => {
     const rows = [];
     for (let i = 0; i < 10; i++) {
-        rows.push(createRow(min, numberBond, i));
+        rows.push(generateRow(min, numberBond, i));
     }
     return rows;
 };
